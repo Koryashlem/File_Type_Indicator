53dcd1de7aa79067515441feaef00baa93d11ecc
Usmann__6771 <usmanahmed6169@gmail.com>
2026-05-23 05:51:42 -0700

    Create magic_db for file type identification
    Added a magic_db dictionary to identify file types based on their magic numbers.

---
+++ signature.py
@@ -0,0 +1,11 @@
+magic_db = {
+    "pdf": [b"%PDF"],
+    "png": [bytes.fromhex("89504E470D0A1A0A")],
+    "jpg": [bytes.fromhex("FFD8FF")],
+    "gif": [b"GIF87a", b"GIF89a"],
+    "mp3": [bytes.fromhex("494433")],
+    "wav": [b"RIFF"],
+    "exe": [b"MZ"],
+    "rar": [bytes.fromhex("526172211A0700")],
+
+}
