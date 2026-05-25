83072a2275f492e4fb478a59b7e4fad353110dc8
Usmann__6771 <usmanahmed6169@gmail.com>
2026-05-25 08:53:47 -0700

    Revise magic_db structure for file signatures
    Updated magic_db to include new file types and format. Also added offsets

--- signature.py
+++ signature.py
@@ -1,11 +1,20 @@
 magic_db = {
-    "pdf": [b"%PDF"],
-    "png": [bytes.fromhex("89504E470D0A1A0A")],
-    "jpg": [bytes.fromhex("FFD8FF")],
-    "gif": [b"GIF87a", b"GIF89a"],
-    "mp3": [bytes.fromhex("494433")],
-    "wav": [b"RIFF"],
-    "exe": [b"MZ"],
-    "rar": [bytes.fromhex("526172211A0700")],
 
+    "pdf": [(0, b"%PDF") ],
+
+    "png": [(0, bytes.fromhex("89504E470D0A1A0A"))],
+
+    "jpg": [(0, bytes.fromhex("FFD8FF"))],
+
+    "gif": [(0, b"GIF87a"),(0, b"GIF89a")],
+
+    "zip": [(0, bytes.fromhex("504B0304"))],
+
+    "rar": [(0, bytes.fromhex("526172211A0700"))],
+
+    "mp3": [(0, bytes.fromhex("494433"))],
+
+    "wav": [(0, b"RIFF")],
+
+    "mp4": [(4, b"ftyp")]
 }
